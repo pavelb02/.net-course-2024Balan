b24bd04dff5641d37743e3041b0e19784c85dcec
Pavel Balan <pavlik17122002@gmail.com>
2024-09-20 22:26:44 +0300

    Добавил конструктор и метод для вывода на консоль.

--- BankSystem.Domain/Models/Currency.cs
+++ BankSystem.Domain/Models/Currency.cs
@@ -1,9 +1,23 @@
-﻿namespace BankSystem.Domain.Models;
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BankSystem.Domain.Models;
 
 public struct Currency
 {
-    public string Code { get; set; }      // Код валюьы (USD, EUR, RUB)
-    public string Name { get; set; }      // Название валюты (доллар США, евро, российский рубль)
-    public string Symbol { get; set; }    // Символ валюты ($, €, ₽)
+    public string Code { get; set; } // Код валюты (USD, EUR, RUB)
+    public string Name { get; set; } // Название валюты (доллар США, евро, российский рубль)
+    public string Symbol { get; set; } // Символ валюты ($, €, ₽)
     public decimal ExchangeRate { get; set; } // Курс валюты
+
+    public Currency(string code, string name, string symbol, decimal exchangeRate)
+    {
+        Code = code;
+        Name = name;
+        Symbol = symbol;
+        ExchangeRate = exchangeRate;
+    }
+    public void Print() => Console.WriteLine($"Код валюты: {Code}, " +
+                                             $"Название валюты: {Name}, " +
+                                             $"Символ валюты: {Symbol}, " +
+                                             $"Курс валюты: {ExchangeRate}");
 }
