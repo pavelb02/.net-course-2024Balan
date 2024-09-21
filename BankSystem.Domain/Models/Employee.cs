e8bfcb84b8f4a614ba069b28a96294782ed604c3
Pavel Balan <pavlik17122002@gmail.com>
2024-09-22 01:41:35 +0300

    Написал конструктор класса и метод Print для отображения на консоли.

--- BankSystem.Domain/Models/Employee.cs
+++ BankSystem.Domain/Models/Employee.cs
@@ -5,17 +5,21 @@ public class Employee : Person
     public Guid EmployeeId { get; set; }
     public string Position { get; set; }
     public DateTime StartDate { get; set; }
-    public decimal Salary { get; set; }
+    public int Salary { get; set; }
     public string Contract { get; set; }
         
-    public Employee(string name, string surname, string position, DateTime startDate, decimal salary)
+    public Employee(string name, string surname, string numPassport, int age, string phone, string position, DateTime startDate, int salary)
+        : base(name, surname, numPassport, age, phone) // ????? ???????????? ???????? ?????? Person
     {
         EmployeeId = Guid.NewGuid();
-        Name = name;
-        Surname = surname;
         Position = position;
         StartDate = startDate;
         Salary = salary;
         Contract =  string.Empty; //?? ????????? ????????????? ? null (?????????, ??????), ??????????? "??????"
     }
+    
+    public void Print() => Console.WriteLine($"Name: {Name}, " +
+                                             $"Surname: {Surname}, " +
+                                             $"Position: {Position}, " +
+                                             $"Salary: {Salary}");
 }
