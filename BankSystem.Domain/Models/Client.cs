7512ec8aab7e7019efc63275b549eb1a0af88048
Pavel Balan <pavlik17122002@gmail.com>
2024-09-22 01:40:26 +0300

    Написал конструтор класса и метод Print для вывода на консоль.

--- BankSystem.Domain/Models/Client.cs
+++ BankSystem.Domain/Models/Client.cs
@@ -1,8 +1,20 @@
 namespace BankSystem.Domain.Models;
 
-class Client : Person
+public class Client : Person
 {
     public Guid ClientId { get; set; }
     public string AccountNumber { get; set; }
     public decimal Balance { get; set; }
+    public Client(string name, string surname, string numPassport, int age, string phone, string accountNumber, decimal balance)
+        : base(name, surname, numPassport, age, phone) // ????? ???????????? ???????? ??????
+    {
+        ClientId = Guid.NewGuid();
+        AccountNumber = accountNumber;
+        Balance = balance;
+    }
+    public void Print() => Console.WriteLine($"Name: {Name}, " +
+                                             $"Surname: {Surname}, " +
+                                             $"AccountNumber: {AccountNumber}, " +
+                                             $"Balance: {Balance} " +
+                                             $"Phone: {Phone}");
 }
