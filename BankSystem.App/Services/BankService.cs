fcfa495361266792b70d1736493dec90d82c4d73
Pavel Balan <pavlik17122002@gmail.com>
2024-09-22 01:38:01 +0300

    Нписал метод расчета зарплаты владельцев и метод преобразования клиента банка в сотрудника.

--- BankSystem.App/Services/BankService.cs
+++ BankSystem.App/Services/BankService.cs
@@ -1,6 +1,26 @@
-﻿namespace BankSystem.App.Services;
+﻿using BankSystem.Domain.Models;
+
+namespace BankSystem.App.Services;
 
 public class BankService
 {
-    
+    public int SalaryCalculation(List<Client> clients, List<Employee> employees, int countOwner)
+    {
+        var profit = clients.Sum(client => 0.2m * client.Balance);
+
+        var costs = employees.Sum(employee => employee.Salary);
+
+        if (countOwner <= 0)
+            throw new ArgumentException("У банка должен быть хотя бы один владелец.");
+        var salaryOwner = (int)((profit - costs) / countOwner);
+        return salaryOwner;
+    }
+
+    public Employee ChangeClientToEmployee(Client client)
+    {
+        var internSolary = 2500;
+        Employee employee = new(client.Name, client.Surname, client.NumPassport, client.Age, client.Phone, "Intern",
+            DateTime.Now, internSolary);
+        return employee;
+    }
 }
