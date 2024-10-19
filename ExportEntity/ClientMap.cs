ee8f3c3fbc78072caad5c6fcc024dd95462973e4
Pavel Balan <pavlik17122002@gmail.com>
2024-10-19 13:38:27 +0300

    FileStream feat: Реализовать методы работы БД с csv-файлами.

---
+++ ExportEntity/ClientMap.cs
@@ -0,0 +1,17 @@
+﻿using BankSystem.Domain.Models;
+using CsvHelper.Configuration;
+
+namespace ExportEntity;
+
+public class ClientMap : ClassMap<Client>
+{
+    public ClientMap()
+    {
+        Map(m => m.Id).Name("Id");
+        Map(m => m.Name).Name("Name");
+        Map(m => m.Surname).Name("Surname");
+        Map(m => m.NumPassport).Name("NumPassport");
+        Map(m => m.Phone).Name("Phone");
+        Map(m => m.DateBirthday).Name("DateBirthday");
+    }
+}
---
+++ ExportEntity/ExportService.cs
@@ -0,0 +1,85 @@
+﻿using System.Globalization;
+using BankSystem.Domain.Models;
+using CsvHelper;
+
+namespace ExportEntity;
+
+public class ExportService
+{
+    private string _pathToDirectory { get; set; }
+    private string _csvFileName { get; set; }
+
+    public ExportService(string pathToDirectory, string csvFileName)
+    {
+        _pathToDirectory = pathToDirectory;
+        _csvFileName = csvFileName;
+    }
+    
+    public void WriteClientsToCsv(List<Client> clients)
+    {
+        DirectoryInfo dirInfo = new DirectoryInfo(_pathToDirectory);
+        if (!dirInfo.Exists)
+        {
+            dirInfo.Create();
+        }
+        string fullPath = Path.Combine(_pathToDirectory, _csvFileName);
+        using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
+        {
+            using (StreamWriter streamWriter = new StreamWriter(fileStream))
+            {
+                using (var writer = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                {
+                    writer.WriteField(nameof(Client.Id));
+                    writer.WriteField(nameof(Client.Name));
+                    writer.WriteField(nameof(Client.Surname));
+                    writer.WriteField(nameof(Client.NumPassport));
+                    writer.WriteField(nameof(Client.Phone));
+                    writer.WriteField(nameof(Client.DateBirthday));
+    
+                    writer.NextRecord();
+
+                    foreach (var client in clients)
+                    {
+                        writer.WriteField(client.Id);
+                        writer.WriteField(client.Name);
+                        writer.WriteField(client.Surname);
+                        writer.WriteField(client.NumPassport);
+                        writer.WriteField(client.Phone);
+                        writer.WriteField(client.DateBirthday.ToString("yyyy-MM-dd"));
+        
+                        writer.NextRecord();
+                    }
+                    writer.Flush();
+                }
+            }
+        }
+    }
+    public List<Client> ReadClientsFromCsv()
+    {
+        string fullPath = Path.Combine(_pathToDirectory, _csvFileName);
+        List<Client> readClients = new List<Client>();
+        using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
+        {
+            using (StreamReader streamReader = new StreamReader(fileStream))
+            {
+                using (var reader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    // 1 вариант
+                    reader.Context.RegisterClassMap<ClientMap>();
+                    readClients = reader.GetRecords<Client>().ToList();
+
+                    // 2 вариант
+                    /*
+                    reader.Read();
+                    reader.ReadHeader();
+                    while (reader.Read())
+                    {
+                        readClients.Add(reader.GetRecord<Client>());
+                    }
+                    */
+                    return readClients;
+                }
+            }
+        }
+    }
+}
